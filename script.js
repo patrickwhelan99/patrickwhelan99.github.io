patrickwhelan99/patrickwhelan99.github.io

commit d6f8ced7bf487e0214ddd2eaee3631ab36204be3

Fixed bug where uploading after graph is already drawn would cause a graphical glitch

script.js
@@ -77,7 +77,7 @@ function readSingleFile(e)
 		let contents = e.target.result;
 		let Obj = JSON.parse(contents);
 		results = analyseData(Obj);
-		showSlides(1);
+		redrawGraphs();
 
 		let yearSliderText = document.getElementById('yearToShowText');
 		let yearSlider = document.getElementById('yearToShowSlider');

commit b5192993d7c3beb8124477dca276fdc2ec072614

Fixed bug introduced by d6f8ced7bf487e0214ddd2eaee3631ab36204be3 which would cause graphs to not be drawn initially

script.js
@@ -77,6 +77,7 @@ function readSingleFile(e)
 		let contents = e.target.result;
 		let Obj = JSON.parse(contents);
 		results = analyseData(Obj);
+		chartsDrawn = true;
 		redrawGraphs();
 
 		let yearSliderText = document.getElementById('yearToShowText');
@@ -88,7 +89,6 @@ function readSingleFile(e)
 
 		var element = document.getElementById('sliderDiv');
 		element.scrollIntoView();
-		chartsDrawn = true;
   	};
 
   	reader.readAsText(file);